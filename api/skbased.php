ba647bdeabe965017d503ea02d483262ad9e54b9
rebinsarbaz81 <64344011+rebinsarbaz81@users.noreply.github.com>
2023-05-12 03:33:49 +0300

    Add files via upload

---
+++ api/skbased.php
@@ -0,0 +1,3 @@
+✘Unknown - You passed an empty string for 'card[number]'. We assume empty values are an attempt to unset a parameter; however 'card[number]' cannot be unset. You should remove 'card[number]' from your request or supply a non-empty value. - You passed an empty string for 'payment_method'. We assume empty values are an attempt to unset a parameter; however 'payment_method' cannot be unset. You should remove 'payment_method' from your request or supply a non-empty value. $1
+
+Bypassing - 7
